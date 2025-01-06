98f601bebb15f67911a9adde7eb48e262d5145ba
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-01-06 10:38:55 -0300

    Adicionei aula 11

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade,MULTADO!')
+} else {
+    console.log(`A sua velocidade é ${vel}Km/h dirija com segurança`)
+}
