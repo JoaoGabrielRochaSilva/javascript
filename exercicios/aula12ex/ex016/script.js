a427408dffe72099fbe1ff37232f559a5927b99a
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-01-07 16:44:56 -0300

    Corrigi o codigo do ex016

--- exercicios/aula12ex/ex016/script.js
+++ exercicios/aula12ex/ex016/script.js
@@ -13,32 +13,38 @@ function verificar() {
         alert('[ERRO] verifique os dados')
     } else {
         if(sex[0].checked) {
-        var genero = 'Homem'
-        } if (idade < 13) {
-        img.setAttribute('src', 'menino.png')
-       } else if (idade <= 25) {
-        img.setAttribute('src', 'homejovem.png')
-       } else if (idade <= 50) {
-        img.setAttribute('src' , 'homem.png')
-       } else {
-        img.setAttribute('src', 'homemidoso.png')
-       }
 
-    } if(sex[1].checked){
-        var genero = 'Mulher'
-        if (idade < 13) {
-        img.setAttribute('src', 'menina.png')
-       } else if (idade <= 25) {
-        img.setAttribute('src', 'mulherjovem.png')
-       } else if (idade <= 50) {
-        img.setAttribute('src' , 'mulher.png')
-       } else {
-        img.setAttribute('src', 'mulheridosa.png')
-       }
+            var genero = 'Homem'
+            if(idade < 13) {
+                img.setAttribute('src', 'menino.png')
+            } else if(idade <= 25) {
+                img.setAttribute('src', 'homejovem.png')
+            } else if(idade < 50) {
+                img.setAttribute('src', 'homem.png')
+            } else {
+                img.setAttribute('src', 'homemidoso.png')
+            }
+
+        } else if(sex[1].checked) {
+
+            var genero = 'Mulher'
+            if(idade < 13) {
+                img.setAttribute('src', 'menina.png')
+            } else if(idade <= 25) {
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if(idade < 50) {
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                img.setAttribute('src', 'mulheridosa.png')
+            }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerText = (`Você é ${genero} e tem ${idade} anos de idade: \n \n`)
+        img.style.border = '1px solid black'
+        img.style.borderRadius = '50%'
+        img.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.400)'
+        res.appendChild(img)
     }
-   
-    res.style.textAlign = 'center'
-    res.innerHTML = (`Você é ${genero}, com ${idade} anos de idade`)
-    res.appendChild(img)
     
 }
