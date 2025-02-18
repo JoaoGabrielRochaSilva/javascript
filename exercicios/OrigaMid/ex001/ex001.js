f5bff6ec808599ce99756a7a8d4c30f534f598ec
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-18 10:03:14 -0300

    ex001 feito

---
+++ exercicios/OrigaMid/ex001/ex001.js
@@ -0,0 +1,15 @@
+var meuNome = 'João Gabriel Rocha Silva';
+var minhaIdade = 17;
+
+var comidaFavorita;
+console.log(comidaFavorita)
+comidaFavorita = 'Arroz, feijão e carne moída.'
+console.log(comidaFavorita)
+
+var meuCarro = 'DogeChalanger',
+    meuEndereço = 461,
+    numeroTelefone,
+    meuTrabalho,
+    numeroDaSorte;
+
+console.log(meuNome,minhaIdade,comidaFavorita,meuCarro,meuEndereço,numeroDaSorte) 
