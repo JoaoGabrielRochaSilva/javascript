afc2bfe8341d48fee1614ea287489d72c0ceec63
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-18 10:52:28 -0300

    ex002 feito

---
+++ exercicios/OrigaMid/tipos-de-dados/ex002.js
@@ -0,0 +1,18 @@
+var novaString = 'Olá';
+console.log(novaString);
+
+var numero = '2025';
+console.log(numero);
+
+var idade = 17;
+console.log(idade);
+
+var nome = 'João Gabriel',
+    sobrenome = 'Rocha Silva',
+    nomeInteiro = `${nome} ${sobrenome}`;
+console.log(nomeInteiro);
+
+var frase = "It's time";
+console.log(frase);
+
+console.log(typeof nomeInteiro)
