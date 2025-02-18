96de5c4066555c8c94c23f109d048087f89004e8
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-18 11:29:54 -0300

    ex003 feito

---
+++ exercicios/OrigaMid/operadores-aritimeticos/ex003.js
@@ -0,0 +1,24 @@
+var expressao = 10 + 5 * 2 / 2 + 20;
+console.log(expressao);
+
+var expressao1 = 'e10' * 5;
+console.log(expressao1);
+
+var valor = '50';
+var unidade1 = 'km';
+var distancia = valor + unidade;
+var distanciaPorDois = distancia / 2;
+console.log(distanciaPorDois);
+
+var num = +'200' + 50;
+console.log(num)
+
+var numero5 = 5
+console.log(++numero5)
+
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade;
+var pesoPorDois = (numero / 2) + unidade;
+console.log(peso)
+console.log(pesoPorDois)
