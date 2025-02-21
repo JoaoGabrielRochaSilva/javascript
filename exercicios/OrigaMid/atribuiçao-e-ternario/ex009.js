54726d5c8f0de57fb92120cab6140ff9fcba1088
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-21 11:46:40 -0300

    ex009 feito

---
+++ exercicios/OrigaMid/atribuiçao-e-ternario/ex009.js
@@ -0,0 +1,10 @@
+var scroll = 1000
+scroll += 500
+
+console.log(scroll)
+
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito = (possuiCarro && possuiCasa) ? 'Dar crédito ao cliente' : 'Negar crédito';
+
+console.log(darCredito)
