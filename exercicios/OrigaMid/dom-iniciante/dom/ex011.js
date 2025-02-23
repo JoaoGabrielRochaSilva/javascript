5b0d8245208ace5c189f3d18ea07b957febed208
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-23 10:00:51 -0300

    Pasta reorganizada e ex011 feito

---
+++ exercicios/OrigaMid/dom-iniciante/dom/ex011.js
@@ -0,0 +1,13 @@
+const url = window.location.href
+console.log(url)
+
+const primeiroElemento = document.querySelector('.ativo')
+console.log(primeiroElemento)
+
+const linguagem = window.navigator.language
+
+console.log(linguagem)
+
+console.log(window.innerWidth)
+
+
