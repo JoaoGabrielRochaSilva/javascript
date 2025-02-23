34f2b168cf5a036819b35658f46cf423c61a932e
João Gabriel Rocha Silva <joaogs.live@gmail.com>
2025-02-23 08:37:22 -0300

    ex010 feito

---
+++ exercicios/OrigaMid/escopo/ex010.js
@@ -0,0 +1,24 @@
+{
+    var cor = 'preto'
+    const marca = 'Fiat'
+    let portas = 4
+    console.log(cor, marca, portas)
+}
+
+const dois = 2;
+function somarDois(x) {
+    return x + dois
+}
+
+function dividirPorDois(x) {
+    return x / dois
+}
+
+console.log(somarDois(4), dividirPorDois(6))
+
+const numero = 50
+for(let contador = 0; contador < 10; contador++) {
+    console.log(contador)
+}
+const total = 10 * numero
+console.log(total)
